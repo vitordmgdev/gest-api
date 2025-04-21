b499a90d07e64ddd9c961fdba759a8885c2c86cb
Vitor <vitordossantosdomingues@gmail.com>
2025-04-21 02:45:17 -0300

    feat: Configura o banco de dados com tabela de usuários e adiciona variáveis de ambiente

---
+++ prisma/migrations/20250421043811_init/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "lastname" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
